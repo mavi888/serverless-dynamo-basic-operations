e1f4642d49b4be416a5a3d886ab6b5c05ab891b6
marcia.villalba <marcia.villalba@gmail.com>
2017-12-11 20:20:38 +0200

    Initial commit

---
+++ databaseManager.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_NAME = process.env.ITEMS_DYNAMODB_TABLE;
+
+module.exports.saveItem = item => {
+  const params = {
+    TableName: TABLE_NAME,
+    Item: item
+  };
+
+  return dynamo.put(params).promise().then(() => {
+    return item.itemId;
+  });
+};
+
+module.exports.getItem = itemId => {
+  const params = {
+    Key: {
+      itemId: itemId
+    },
+    TableName: TABLE_NAME
+  };
+
+  return dynamo.get(params).promise().then(result => {
+    return result.Item;
+  });
+};
+
+module.exports.deleteItem = itemId => {
+  const params = {
+    Key: {
+      itemId: itemId
+    },
+    TableName: TABLE_NAME
+  };
+
+  return dynamo.delete(params).promise();
+};
+
+module.exports.updateItem = (itemId, paramsName, paramsValue) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      itemId
+    },
+    ConditionExpression: 'attribute_exists(itemId)',
+    UpdateExpression: 'set ' + paramsName + ' = :v',
+    ExpressionAttributeValues: {
+      ':v': paramsValue
+    },
+    ReturnValues: 'ALL_NEW'
+  };
+
+  return dynamo.update(params).promise().then(response => {
+    return response.Attributes;
+  });
+};
---
+++ handler.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const databaseManager = require('./databaseManager');
+const uuidv1 = require('uuid/v1');
+
+function createResponse(statusCode, message) {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  };
+}
+
+module.exports.saveItem = (event, context, callback) => {
+  const item = JSON.parse(event.body);
+  console.log(item);
+  item.itemId = uuidv1();
+
+  databaseManager.saveItem(item).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
+
+module.exports.getItem = (event, context, callback) => {
+  const itemId = event.pathParameters.itemId;
+
+  databaseManager.getItem(itemId).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
+
+module.exports.deleteItem = (event, context, callback) => {
+  const itemId = event.pathParameters.itemId;
+
+  databaseManager.deleteItem(itemId).then(response => {
+    callback(null, createResponse(200, 'Item was deleted'));
+  });
+};
+
+module.exports.updateItem = (event, context, callback) => {
+  const itemId = event.pathParameters.itemId;
+
+  const body = JSON.parse(event.body);
+  const paramName = body.paramName;
+  const paramValue = body.paramValue;
+
+  databaseManager.updateItem(itemId, paramName, paramValue).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
